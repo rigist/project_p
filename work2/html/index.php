fba1962e2887184ecff86e5a1fa5082c717e945c
Ilgiz Bikshanov <66265205+rigist@users.noreply.github.com>
2022-12-08 14:28:23 +0500

    Update index.php

--- work2/html/index.php
+++ work2/html/index.php
@@ -1,18 +1,26 @@
 <?php
 include "../src/functions.php";
-
+echo "<br>";
 task1(["a", "b"], false);
 
+echo "<br>";
 echo task2('+', 1, 2, 3, 4);
+echo "<br>";
 echo task2('-', 1, 2, 3, 4);
+echo "<br>";
 echo task2('/', 1, 2, 3, 4);
+echo "<br>";
 echo task2('*', 1, 2, 3, 4);
 
+echo "<br>";
 task3(2,2);
+echo "<br>";
 task3(-2,2);
 
+echo "<br>";
 task4();
-
+echo "<br>";
 task5();
-
+echo "<br>";
 task6();
+echo "<br>";
